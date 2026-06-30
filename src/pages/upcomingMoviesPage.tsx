d94eca249c7eb19a3fc41a3a318a453de3820690
Vavarussle <vlad.mihalachita@gmail.com>
2026-06-30 19:39:48 +0100

    Fix upcoming movies page.

--- src/pages/upcomingMoviesPage.tsx
+++ src/pages/upcomingMoviesPage.tsx
@@ -1,24 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from "../components/templateMovieListPage";
-import { BaseMovieProps } from "../types/interfaces";
 import { getUpcomingMovies } from "../api/tmdb-api";
+import { DiscoverMovies, BaseMovieProps } from "../types/interfaces";
+import { useQuery } from "react-query";
+import Spinner from "../components/spinner";
+import AddToFavouritesIcon from "../components/cardIcons/addToFavourites";
 
 const UpcomingMoviesPage: React.FC = () => {
-  const [movies, setMovies] = useState<BaseMovieProps[]>([]);
+  const { data, error, isLoading, isError } = useQuery<DiscoverMovies, Error>(
+    "upcoming",
+    getUpcomingMovies
+  );
 
-  const toDo = () => true;
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  useEffect(() => {
-    getUpcomingMovies().then((movies) => {
-      setMovies(movies);
-    });
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data ? data.results : [];
 
   return (
     <PageTemplate
       title="Upcoming Movies"
       movies={movies}
-      selectFavourite={toDo}
+      action={(movie: BaseMovieProps) => {
+        return <AddToFavouritesIcon {...movie} />;
+      }}
     />
   );
 };
